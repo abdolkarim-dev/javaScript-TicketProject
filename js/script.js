8533febe824f70dc8d384c1fb1fd355846bcd625
AbdOlkarim Bazdar <bazdarabdolkarim@gmail.com>
2026-01-05 06:16:13 +0330

    feat:select country and show city for ticket

---
+++ js/script.js
@@ -0,0 +1,22 @@
+﻿//  (: همین الان شروع کنین و بنویسین
+// چششششم
+//اشتباه من این بود که میخاستم با فور ایچ روی ابجکت ها حرکت کنم درحالی که نیاز به یک ارایه داشت
+let cities = { 
+    Iran : ['isfahan','shiraz','tehran'],
+    US : ['new york','los angeles','san antonio'],
+    Canada : ['toronto','vancouver','montreal']
+}
+
+let selectOptionCities = document.getElementById('countrySelect')
+let selectOptionCitiesResult = document.getElementById('citySelect')
+function countrySelectFunction(){ 
+    selectOptionCitiesResult.innerHTML = ''
+    let selectedCountry = selectOptionCities.value 
+    if (cities[selectedCountry]){
+        cities[selectedCountry].forEach(function(city){
+           selectOptionCitiesResult.innerHTML += '<option>'+city+'</option>'
+        });
+    }else{
+        selectOptionCitiesResult.innerHTML = '<option> Plese Select Cities </option>'
+    }
+}
